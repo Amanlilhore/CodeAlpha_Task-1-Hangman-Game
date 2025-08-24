07a65a39ad1a406bf877baee5954b6662af3b449
Amanlilhore <amanlilhore@gmail.com>
2025-08-24 21:55:23 +0530

    Add files via upload

---
+++ wordslist.py
@@ -0,0 +1,18 @@
+# word for hangman game
+
+words = [
+    "apple","grape","mango","pearl","stone","chair","bread","green","train","smile",
+    "water","cloud","river","plant","house","mouse","light","night","table","clock",
+    "earth","world","beach","storm","candy","sugar","movie","pizza","juice","dance",
+    "happy","music","phone","books","queen","tiger","horse","zebra","sheep","beard",
+    "planet","guitar","orange","school","doctor","friend","animal","silver","castle","winter",
+    "butter","bridge","rocket","forest","people","button","stream","little","charge","puzzle",
+    "flower","window","bucket","dragon","mother","father","border","circle","energy","throne",
+    "library","diamond","morning","teacher","monster","country","passion","journey","picture","station",
+    "captain","victory","silence","balance","battery","fashion","palace","orchard","fantasy","charity",
+    "sandlot","goblins","compass","harvest","sunrise","sunset","village","college","resolve","kingdom",
+    "quirky","zephyr","mythic","riddle","sphinx","cipher","magnet","cobalt","ember","raven",
+    "thrift","plasma","quasar","ginger","anchor","hazeln","quartz","fluent","crispy","velvet"
+]
+
+
